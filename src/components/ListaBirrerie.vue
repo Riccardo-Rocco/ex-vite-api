463054ea4defb8e3abc7b2f461fe3b0db91eb4cd
Riccardo <riccardorocco2001@gmail.com>
2023-11-30 17:35:23 +0100

    fine 1parte

---
+++ src/components/ListaBirrerie.vue
@@ -0,0 +1,36 @@
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      birrerie: [],
+    };
+  },
+  mounted() {
+    axios.get('https://api.openbrewerydb.org/v1/breweries?by_country=poland&per_page=10')
+      .then(response => {
+        this.birrerie = response.data;
+      })
+      
+  },
+};
+</script>
+
+
+<template>
+  <div>
+    <h1>Elenco delle Birrerie in Polonia</h1>
+    <ul>
+      <li v-for="birreria in birrerie" :key="birreria.id">
+        {{ birreria.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+
+
+<style>
+
+</style>
