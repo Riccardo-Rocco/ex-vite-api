ff746692748b92f4d4ad06e94b7a57b62fdeb1ae
Riccardo <riccardorocco2001@gmail.com>
2023-12-01 17:56:16 +0100

    commit: fine progetto con input

--- src/components/ListaBirrerie.vue
+++ src/components/ListaBirrerie.vue
@@ -5,14 +5,35 @@ export default {
   data() {
     return {
       birrerie: [],
+      searchTerm: '', 
     };
   },
   mounted() {
-    axios.get('https://api.openbrewerydb.org/v1/breweries?by_country=poland&per_page=10')
-      .then(response => {
-        this.birrerie = response.data;
-      })
-      
+    this.fetchBirrerie();
+  },
+  methods: {
+    fetchBirrerie() {
+      axios.get('https://api.openbrewerydb.org/v1/breweries?by_country=poland&per_page=10')
+        .then(response => {
+          this.birrerie = response.data;
+        })
+        
+    },
+   
+    search() {
+    
+      if (this.searchTerm.trim() !== '') {
+        
+        this.birrerie = this.birrerie.filter(birreria =>
+          Object.values(birreria).some(value =>
+            String(value).toLowerCase().includes(this.searchTerm.toLowerCase())
+          )
+        );
+      } else {
+        
+        this.fetchBirrerie();
+      }
+    },
   },
 };
 </script>
@@ -20,19 +41,23 @@ export default {
 <template>
   <div>
     <h1>Elenco delle Birrerie in Polonia</h1>
+    
+    
+    <input v-model="searchTerm" @input="search" placeholder="Inserisci il termine di ricerca">
+   
+    <button @click="search">Cerca</button>
+    
     <ul>
       <li v-for="birreria in birrerie" :key="birreria.id">
         <h2>{{ birreria.name }}</h2>
         <p>Posizione: {{ birreria.country }}</p>
         <p>Coordinate: {{ birreria.latitude }}, {{ birreria.longitude }}</p>
-        <p>Indirizzo: {{ birreria.adress1 }}, {{ birreria.city }}, {{ birreria.state_province }}, {{ birreria.postal_code }}</p>
+        <p>Indirizzo: {{ birreria.address_1 }}, {{ birreria.city }}, {{ birreria.state_province }}, {{ birreria.postal_code }}</p>
         <hr>
       </li>
     </ul>
   </div>
 </template>
-
-
 
 <style>
 
